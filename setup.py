b96c17cba2ddafacd522e5313ee2166c7f50f09b
Ralph Meijer <ralphm@ik.nu>
2013-09-24 15:20:42 +0200

    Fixup install.
     * Install twistd plugin.
     * Install RabbitMQ AMQP protocol spec file.
     * Add MANIFEST.in for source distributions.
     * Add run-time dependencies.
    
    Addresses: mochi/udplog#1

--- setup.py
+++ setup.py
@@ -1,6 +1,28 @@
 import os
 from setuptools import setup
 
+# Make sure 'twisted' doesn't appear in top_level.txt
+
+try:
+    from setuptools.command import egg_info
+    egg_info.write_toplevel_names
+except (ImportError, AttributeError):
+    pass
+else:
+    def _top_level_package(name):
+        return name.split('.', 1)[0]
+
+    def _hacked_write_toplevel_names(cmd, basename, filename):
+        pkgs = dict.fromkeys(
+            [_top_level_package(k)
+                for k in cmd.distribution.iter_distribution_names()
+                if _top_level_package(k) != "twisted"
+            ]
+        )
+        cmd.write_file("top-level names", filename, '\n'.join(pkgs) + '\n')
+
+    egg_info.write_toplevel_names = _hacked_write_toplevel_names
+
 # Utility function to read the README file.
 def read(fname):
     return open(os.path.join(os.path.dirname(__file__), fname)).read()
@@ -15,11 +37,27 @@ setup(
     license = "MIT",
     keywords = "logging twisted udp scribe",
     url = "https://github.com/mochi/udplog",
-    packages=['udplog'],
+    packages=[
+        'udplog',
+        'udplog.test',
+        'twisted.plugins',
+    ],
+    package_data={
+        'twisted.plugins': ['twisted/plugins/server.py'],
+        'udplog': ['amqp0-9-1.extended.xml'],
+    },
+    zip_safe=False,
     long_description=read('README.rst'),
     classifiers=[
         "Development Status :: 4 - Beta",
         "Topic :: Utilities",
         "License :: OSI Approved :: MIT License",
+    ],
+    install_requires=[
+        'simplejson',
+        'Twisted >= 13.0.0',
+        'txAMQP',
+        'thrift',
+        'scribe',
     ],
 )
