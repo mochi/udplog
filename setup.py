b6efef4d0519e1c8fd394e2da35af58e7cd37fc6
Christopher Zorn <christopher@mochimedia.com>
2013-09-24 10:22:00 -0700

    double authors

--- setup.py
+++ setup.py
@@ -30,7 +30,8 @@ def read(fname):
 setup(
     name = "udplog",
     version = "0.0.1",
-    author = "Mochi Media",
+    author = "Mochi Media, Ralph Meijer",
+    author_email = "dev@mochimedia.com, ralphm@ik.nu",
     description = ("UDPLog is a system for emitting application log events"
                    " via UDP and shipping them via RabbitMQ or Scribe for "
                    "further processing."),
