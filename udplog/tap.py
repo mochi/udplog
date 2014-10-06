496980759d1756039d045c2e59006d94001c75be
Christopher Zorn <czorn@machinezone.com>
2014-10-06 12:59:17 -0700

    force type values in options

--- udplog/tap.py
+++ udplog/tap.py
@@ -20,18 +20,18 @@ from udplog import udplog
 class Options(usage.Options):
     optParameters = [
         ('udplog-interface', None, udplog.DEFAULT_HOST, 'UDPLog interface'),
-        ('udplog-port', None, udplog.DEFAULT_PORT, 'UDPLog port'),
+        ('udplog-port', None, udplog.DEFAULT_PORT, 'UDPLog port', int),
 
         ('scribe-host', None, None, 'Scribe Thrift host'),
-        ('scribe-port', None, 1463, 'Scribe Thrift port'),
+        ('scribe-port', None, 1463, 'Scribe Thrift port', int),
 
         ('rabbitmq-host', None, None, 'RabbitMQ host'),
-        ('rabbitmq-port', None, 5672, 'RabbitMQ host'),
+        ('rabbitmq-port', None, 5672, 'RabbitMQ host', int),
         ('rabbitmq-vhost', None, '/', 'RabbitMQ virtual host'),
         ('rabbitmq-exchange', None, 'logs', 'RabbitMQ exchange'),
         ('rabbitmq-queue-size', None, 2500,
-             'Maximum number of log events to buffer for RabbitMQ'),
-        ('redis-port', None, 6379, 'Redis port'),
+             'Maximum number of log events to buffer for RabbitMQ', int),
+        ('redis-port', None, 6379, 'Redis port', int),
         ('redis-key', None, None, 'Redis list key'),
         ]
 
@@ -59,7 +59,7 @@ def makeService(config):
     # Set up UDP server as the dispatcher.
     dispatcher = DispatcherFromUDPLogProtocol()
 
-    udplogServer = internet.UDPServer(port=int(config['udplog-port']),
+    udplogServer = internet.UDPServer(port=config['udplog-port'],
                                       protocol=dispatcher,
                                       interface=config['udplog-interface'],
                                       maxPacketSize=65536)
@@ -71,7 +71,7 @@ def makeService(config):
         factory = UDPLogClientFactory(scribe.ScribeProtocol,
                                       dispatcher)
         scribeClient = internet.TCPClient(config['scribe-host'],
-                                          int(config['scribe-port']),
+                                          config['scribe-port'],
                                           factory)
         scribeClient.setServiceParent(s)
 
@@ -84,7 +84,7 @@ def makeService(config):
             exchange=config['rabbitmq-exchange'],
             queueSize=config['rabbitmq-queue-size'])
         rabbitmqClient = internet.TCPClient(config['rabbitmq-host'],
-                                            int(config['rabbitmq-port']),
+                                            config['rabbitmq-port'],
                                             factory)
         rabbitmqClient.setServiceParent(s)
 
