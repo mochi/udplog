b1f0e45d6faf53e7a50e7418f54953914f66a2b4
Ralph Meijer <ralphm@ik.nu>
2015-02-09 17:06:16 +0100

    Typo

--- udplog/tap.py
+++ udplog/tap.py
@@ -80,8 +80,7 @@ def makeService(config):
     if (config.get('syslog-port') is not None or
         config.get('syslog-unix-socket') is not None):
         hostname = socket.gethostname()
-        hostnames = {hostname.split('.')[0],
-                     hostname}
+        hostnames = {hostname.split('.')[0]: hostname}
         syslogProtocol = syslog.SyslogDatagramProtocol(
             dispatcher.eventReceived, hostnames=hostnames)
 
